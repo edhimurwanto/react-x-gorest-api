bbb4d15a1af27867175bfa03304c00225c10ef0f
Edi Murwanto <edhi.uchiha@gmail.com>
2021-09-15 15:16:44 +0700

    Example using context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import {ThemeContext, themes} from './theme/ThemeContext';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const toggleTheme = (newTheme) => {
+
+        setTheme({
+            ...theme,
+            theme: newTheme
+        })
+    }
+
+    const [theme, setTheme] = useState({
+        theme: themes.dark,
+        toggleTheme: toggleTheme
+    });
+
+    return (
+        <ThemeContext.Provider value={theme}>
+            <div className="App">
+                <Toolbar/>
+            </div>
+        </ThemeContext.Provider>
+    );
+}
+
+const Toolbar = () => {
+    return (
+        <div>
+            <Button/>
+        </div>
+    )
+}
+
+const Button = () => {
+
+    return (
+        <ThemeContext.Consumer>
+            {
+                ({theme, toggleTheme}) => {
+                    return (
+                        <div style={{backgroundColor: '#fff'}}>
+                            <button type='button'
+                                    style={{backgroundColor: theme.background, color: theme.foreground}}
+                                    onClick={() => {
+                                        const newTheme = theme === themes.dark ? themes.light : themes.dark;
+                                        toggleTheme(newTheme);
+                                    }}
+                            >BUTTON
+                            </button>
+                        </div>
+                    )
+                }
+            }
+        </ThemeContext.Consumer>
+    )
+
 }
 
 export default App;
